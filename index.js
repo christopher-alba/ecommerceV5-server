f10294ee926091b28afa4cd3044c661ce8007ba0
christopher-alba <calb200@aucklanduni.ac.nz>
2021-06-20 15:42:20 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { resolve } = require("path");
+const { readFileSync } = require("fs");
+const { resolvers } = require("./graphql/resolvers");
+
+const typeDefs = gql(
+  readFileSync(resolve("./graphql/schema.graphql"), {
+    encoding: "utf8",
+  })
+);
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen(5000 || process.env.PORT).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
