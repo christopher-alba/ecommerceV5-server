6ab23da260cba87cf16dd180dc1bb93924d9212b
Christopher <calb200@aucklanduni.ac.nz>
2021-06-20 17:05:28 +1200

    create models for mongodb

--- mongodb/models.js
+++ mongodb/models.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
+const { productSchema, userSchema, cartSchema } = require("./schema");
 
+const Product = mongoose.model("Product", productSchema);
+const User = mongoose.model("User", userSchema);
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = {
+  Product,
+  User,
+  Cart,
+};
